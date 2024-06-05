0ea83653b4e3a5bde19ac7b66d5706620ed4f406
maple@max <feng.liang@kaust.edu.sa>
2024-06-05 16:55:48 +0800

    0.23 upstream merge fix part 2:
    * fix eventsource-stream import error

--- async-openai-wasm/src/client.rs
+++ async-openai-wasm/src/client.rs
@@ -460,7 +460,8 @@ impl<C: Config> Client<C> {
             .eventsource()
             .unwrap();
 
-        stream_mapped_raw_events(event_source, event_mapper).await
+        // stream_mapped_raw_events(event_source, event_mapper).await
+        todo!()
     }
 
     /// Make HTTP GET request to receive SSE
@@ -542,50 +543,50 @@ impl<O: DeserializeOwned + Send + 'static> Stream for OpenAIEventStream<O> {
     }
 }
 
-pub(crate) async fn stream_mapped_raw_events<O>(
-    mut event_source: EventSource,
-    event_mapper: impl Fn(eventsource_stream::Event) -> Result<O, OpenAIError> + Send + 'static,
-) -> Pin<Box<dyn Stream<Item=Result<O, OpenAIError>> + Send>>
-    where
-        O: DeserializeOwned + std::marker::Send + 'static,
-{
-    let (tx, rx) = tokio::sync::mpsc::unbounded_channel();
-
-    tokio::spawn(async move {
-        while let Some(ev) = event_source.next().await {
-            match ev {
-                Err(e) => {
-                    if let Err(_e) = tx.send(Err(OpenAIError::StreamError(e.to_string()))) {
-                        // rx dropped
-                        break;
-                    }
-                }
-                Ok(event) => match event {
-                    Event::Message(message) => {
-                        let mut done = false;
-
-                        if message.data == "[DONE]" {
-                            done = true;
-                        }
-
-                        let response = event_mapper(message);
-
-                        if let Err(_e) = tx.send(response) {
-                            // rx dropped
-                            break;
-                        }
-
-                        if done {
-                            break;
-                        }
-                    }
-                    Event::Open => continue,
-                },
-            }
-        }
-
-        event_source.close();
-    });
-
-    Box::pin(tokio_stream::wrappers::UnboundedReceiverStream::new(rx))
-}
+// pub(crate) async fn stream_mapped_raw_events<O>(
+//     mut event_source: EventSource,
+//     event_mapper: impl Fn(eventsource_stream::Event) -> Result<O, OpenAIError> + Send + 'static,
+// ) -> Pin<Box<dyn Stream<Item=Result<O, OpenAIError>> + Send>>
+//     where
+//         O: DeserializeOwned + std::marker::Send + 'static,
+// {
+//     let (tx, rx) = tokio::sync::mpsc::unbounded_channel();
+//
+//     tokio::spawn(async move {
+//         while let Some(ev) = event_source.next().await {
+//             match ev {
+//                 Err(e) => {
+//                     if let Err(_e) = tx.send(Err(OpenAIError::StreamError(e.to_string()))) {
+//                         // rx dropped
+//                         break;
+//                     }
+//                 }
+//                 Ok(event) => match event {
+//                     Event::Message(message) => {
+//                         let mut done = false;
+//
+//                         if message.data == "[DONE]" {
+//                             done = true;
+//                         }
+//
+//                         let response = event_mapper(message);
+//
+//                         if let Err(_e) = tx.send(response) {
+//                             // rx dropped
+//                             break;
+//                         }
+//
+//                         if done {
+//                             break;
+//                         }
+//                     }
+//                     Event::Open => continue,
+//                 },
+//             }
+//         }
+//
+//         event_source.close();
+//     });
+//
+//     Box::pin(tokio_stream::wrappers::UnboundedReceiverStream::new(rx))
+// }
