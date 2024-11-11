4605113999709da6ae621058e37873311bbb7681
sharif <61516383+sharifhsn@users.noreply.github.com>
2024-11-12 04:19:49 +0800

    Add support for organization API (#265)
    * Implement `invites`
    
    * Consistently inline format
    
    * Implement `users`
    
    * Replace `InviteRole` with generic `OrganizationRole`
    
    * Implement `projects`
    
    * Update requests to use builder
    
    * Remove `default` from builder
    
    * Update `projects` to include verification information (https://help.openai.com/en/articles/9824607-api-platform-verifications)
    
    * Implement `project_users`
    
    ---------
    
    Co-authored-by: Himanshu Neema <himanshun.iitkgp@gmail.com>
    
    (cherry picked from commit ac204a3b58844caa82a199d598202283e3266f83)

--- async-openai-wasm/src/types/common.rs
+++ async-openai-wasm/src/types/common.rs
@@ -1,7 +1,15 @@
 use bytes::Bytes;
+use serde::{Deserialize, Serialize};
 
 #[derive(Debug, Clone, PartialEq)]
 pub enum InputSource {
     Bytes { filename: String, bytes: Bytes },
     VecU8 { filename: String, vec: Vec<u8> },
+}
+
+#[derive(Debug, Serialize, Deserialize, Clone, Copy, PartialEq)]
+#[serde(rename_all = "snake_case")]
+pub enum OrganizationRole {
+    Owner,
+    Reader,
 }
