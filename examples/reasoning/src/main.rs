66e2d21b75c75737543b1a20ab167bf0c0294e4e
maple@max <feng.liang@kaust.edu.sa>
2025-02-19 19:53:04 +0800

    fix example

--- examples/reasoning/src/main.rs
+++ examples/reasoning/src/main.rs
@@ -8,38 +8,49 @@ use serde_json::json;
 
 const OPENROUTER_REASONING_KEY: &str = "reasoning";
 const OPENROUTER_BASEURL: &str = "https://openrouter.ai/api/v1";
+const OPENROUTER_MODEL_NAME: &str = "deepseek/deepseek-r1";
 const DEEPSEEK_REASONING_KEY: &str = "reasoning_content";
 const DEEPSEEK_BASEURL: &str = "https://api.deepseek.com";
+const DEEPSEEK_MODEL_NAME: &str = "deepseek-reasoner";
 
 
 #[tokio::main(flavor = "current_thread")]
 async fn main() {
     let test_key = std::env::var("TEST_API_KEY").unwrap();
     let use_deepseek = std::env::var("USE_DEEPSEEK").is_ok();
-    let (reasoning_key, base_url) = if use_deepseek {
-        (DEEPSEEK_REASONING_KEY, DEEPSEEK_BASEURL)
+    let (reasoning_key, base_url, model_name) = if use_deepseek {
+        (DEEPSEEK_REASONING_KEY, DEEPSEEK_BASEURL, DEEPSEEK_MODEL_NAME)
     } else {
-        (OPENROUTER_REASONING_KEY, OPENROUTER_BASEURL)
+        (OPENROUTER_REASONING_KEY, OPENROUTER_BASEURL, OPENROUTER_MODEL_NAME)
     };
     let client = Client::with_config(
         OpenAIConfig::new()
             .with_api_base(base_url)
             .with_api_key(test_key),
     );
-    let request = CreateChatCompletionRequestArgs::default()
-        .messages(vec![ChatCompletionRequestUserMessageArgs::default()
-            .content("Hello! Do you know the Rust programming language?")
+    let messages = vec![ChatCompletionRequestUserMessageArgs::default()
+        .content("Hello! Do you know the Rust programming language?")
+        .build()
+        .unwrap()
+        .into()];
+    let request = if use_deepseek {
+        CreateChatCompletionRequestArgs::default()
+            .messages(messages)
+            .model(model_name)
             .build()
             .unwrap()
-            .into()])
-        .model("deepseek/deepseek-r1")
-        // The extra params that OpenRouter requires to get reasoning content
-        // See https://openrouter.ai/docs/api-reference/parameters#include-reasoning
-        .extra_params(json!({
-            "include_reasoning" : true
-        }))
-        .build()
-        .unwrap();
+    } else {
+        CreateChatCompletionRequestArgs::default()
+            .messages(messages)
+            .model(model_name)
+            // The extra params that OpenRouter requires to get reasoning content
+            // See https://openrouter.ai/docs/api-reference/parameters#include-reasoning
+            .extra_params(json!({
+                "include_reasoning" : true
+            }))
+            .build()
+            .unwrap()
+    };
 
     let mut result = client.chat().create_stream(request).await.unwrap();
     let mut reasoning = String::new();
