7ab615e638b5ef713382c7fcbd20490429261459
Makoto <36318069+makotoe@users.noreply.github.com>
2025-10-15 09:19:34 +0800

    Add support for expires_after field in CreateFileRequest and expires_at field in OpenAIFile (#444)
    * Add CreateFileRequest
    
    * FileExpiresAfterAnchor
    
    * Add OpenAIFile::expires_at
    
    * Add FileExpiresAfter to AsyncTryFrom<CreateFileRequest>
    
    * Change OpenAIFile::expires_at type
    
    * Make CreateFileRequest::expires_after optional
    
    (cherry picked from commit fe55ff66f6c199b9883cca6ca99ed3e5a9b55922)

--- async-openai-wasm/src/types/impls.rs
+++ async-openai-wasm/src/types/impls.rs
@@ -17,7 +17,7 @@ use super::{
     CreateTranscriptionRequest, CreateTranslationRequest, DallE2ImageSize, EmbeddingInput,
     FileInput, FilePurpose, FunctionName, ImageInput, ImageModel, ImageResponseFormat, ImageSize,
     ImageUrl, ModerationInput, Prompt, Role, Stop, TimestampGranularity,
-    responses::{CodeInterpreterContainer, Input, InputContent, Role as ResponsesRole},
+    responses::{CodeInterpreterContainer, Input, InputContent, Role as ResponsesRole}, FileExpiresAfterAnchor
 };
 use crate::traits::AsyncTryFrom;
 use crate::{error::OpenAIError, types::InputSource, util::create_file_part};
@@ -257,6 +257,18 @@ impl Display for FilePurpose {
                 Self::Batch => "batch",
                 Self::FineTune => "fine-tune",
                 Self::Vision => "vision",
+            }
+        )
+    }
+}
+
+impl Display for FileExpiresAfterAnchor {
+    fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
+        write!(
+            f,
+            "{}",
+            match self {
+                Self::CreatedAt => "created_at",
             }
         )
     }
@@ -889,9 +901,14 @@ impl AsyncTryFrom<CreateFileRequest> for reqwest::multipart::Form {
 
     async fn try_from(request: CreateFileRequest) -> Result<Self, Self::Error> {
         let file_part = create_file_part(request.file.source).await?;
-        let form = reqwest::multipart::Form::new()
+        let mut form = reqwest::multipart::Form::new()
             .part("file", file_part)
             .text("purpose", request.purpose.to_string());
+
+        if let Some(expires_after) = request.expires_after {
+            form = form.text("expires_after[anchor]", expires_after.anchor.to_string())
+                .text("expires_after[seconds]", expires_after.seconds.to_string());
+        }
         Ok(form)
     }
 }
