64bit/async-openai

feat: sync audio api + fix error types (#464)
* updates to CreateSpeechRequest

* types::audio; streaming audio types and api

* fix imports in the examples

* fix import

* fix api; implement trait

* add audio-speech-stream example

* updated CreateTranscriptionRequest

* updates for CreateTranscriptionResponseJson

* udpated CreateTranscriptionResponseVerboseJson

* updated CreateTranscriptionResponseDiarizedJson

* update types for diarized

* update transcription example

* streaming from form submission

* add streaming example to audio-transcribe

* updates for translation

* update to example

* update audio api groups

* update audio examples

* fix webhooks error type

* fix errors reported by clippy

* fix based on clippy

* fix for: https://rust-lang.github.io/rust-clippy/master/index.html\#result_large_err

* cargo fmt

async-openai/tests/whisper.rs
@@ -1,17 +1,6 @@
-use async_openai::types::CreateTranslationRequestArgs;
-use async_openai::{types::CreateTranscriptionRequestArgs, Client};
+use async_openai::types::audio::CreateTranslationRequestArgs;
+use async_openai::{types::audio::CreateTranscriptionRequestArgs, Client};
 use tokio_test::assert_err;
-
-#[tokio::test]
-async fn transcribe_test() {
-    let client = Client::new();
-
-    let request = CreateTranscriptionRequestArgs::default().build().unwrap();
-
-    let response = client.audio().transcribe(request).await;
-
-    assert_err!(response); // FileReadError("cannot extract file name from ")
-}
 
 #[tokio::test]
 async fn transcribe_sendable_test() {
@@ -21,21 +10,10 @@ async fn transcribe_sendable_test() {
     let transcribe = tokio::spawn(async move {
         let request = CreateTranscriptionRequestArgs::default().build().unwrap();
 
-        client.audio().transcribe(request).await
+        client.audio().transcription().create(request).await
     });
 
     let response = transcribe.await.unwrap();
-
-    assert_err!(response); // FileReadError("cannot extract file name from ")
-}
-
-#[tokio::test]
-async fn translate_test() {
-    let client = Client::new();
-
-    let request = CreateTranslationRequestArgs::default().build().unwrap();
-
-    let response = client.audio().translate(request).await;
 
     assert_err!(response); // FileReadError("cannot extract file name from ")
 }
@@ -48,7 +26,7 @@ async fn translate_sendable_test() {
     let translate = tokio::spawn(async move {
         let request = CreateTranslationRequestArgs::default().build().unwrap();
 
-        client.audio().translate(request).await
+        client.audio().translation().create(request).await
     });
 
     let response = translate.await.unwrap();

ifsheldon/async-openai-wasm

(cherry picked from commit eaa396bfdf7944a317f8e0de880f064a1ff23bb1)

async-openai/tests/whisper.rs
@@ -1,18 +1,6 @@
+use async_openai_wasm::types::audio::CreateTranslationRequestArgs;
+use async_openai_wasm::{types::audio::CreateTranscriptionRequestArgs, Client};
 use tokio_test::assert_err;
-
-use async_openai_wasm::types::CreateTranslationRequestArgs;
-use async_openai_wasm::{Client, types::CreateTranscriptionRequestArgs};
-
-#[tokio::test]
-async fn transcribe_test() {
-    let client = Client::new();
-
-    let request = CreateTranscriptionRequestArgs::default().build().unwrap();
-
-    let response = client.audio().transcribe(request).await;
-
-    assert_err!(response); // FileReadError("cannot extract file name from ")
-}
 
 #[tokio::test]
 async fn transcribe_sendable_test() {
@@ -22,21 +10,10 @@ async fn transcribe_sendable_test() {
     let transcribe = tokio::spawn(async move {
         let request = CreateTranscriptionRequestArgs::default().build().unwrap();
 
-        client.audio().transcribe(request).await
+        client.audio().transcription().create(request).await
     });
 
     let response = transcribe.await.unwrap();
-
-    assert_err!(response); // FileReadError("cannot extract file name from ")
-}
-
-#[tokio::test]
-async fn translate_test() {
-    let client = Client::new();
-
-    let request = CreateTranslationRequestArgs::default().build().unwrap();
-
-    let response = client.audio().translate(request).await;
 
     assert_err!(response); // FileReadError("cannot extract file name from ")
 }
@@ -49,7 +26,7 @@ async fn translate_sendable_test() {
     let translate = tokio::spawn(async move {
         let request = CreateTranslationRequestArgs::default().build().unwrap();
 
-        client.audio().translate(request).await
+        client.audio().translation().create(request).await
     });
 
     let response = translate.await.unwrap();