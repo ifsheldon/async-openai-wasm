c0037d8d10044c84c1ddadf4f0f3bb8519c694c1
Himanshu Neema <himanshun.iitkgp@gmail.com>
2025-11-20 08:03:50 +0800

    feat: new admin apis: groups and roles (#481)
    * add group and role management apis
    
    * updated spec
    
    * update readme
    
    * updated reamde
    
    * cargo fmt
    
    (cherry picked from commit a78cc340de34a6d909e206cd2c474261a883bb3e)

--- async-openai/src/admin.rs
+++ async-openai/src/admin.rs
@@ -1,10 +1,10 @@
 use crate::{
-    Client, admin_api_keys::AdminAPIKeys, audit_logs::AuditLogs, certificates::Certificates,
-    config::Config, invites::Invites, projects::Projects, users::Users,
+    admin_api_keys::AdminAPIKeys, audit_logs::AuditLogs, certificates::Certificates,
+    config::Config, invites::Invites, projects::Projects, users::Users, Client, groups::Groups, roles::Roles,
 };
 
 /// Admin group for all administration APIs.
-/// This groups together admin API keys, invites, users, projects, audit logs, and certificates.
+/// This groups together admin API keys, invites, users, projects, audit logs, certificates, roles, and groups.
 pub struct Admin<'c, C: Config> {
     client: &'c Client<C>,
 }
@@ -42,5 +42,15 @@ impl<'c, C: Config> Admin<'c, C> {
     /// To call [Certificates] group related APIs using this client.
     pub fn certificates(&self) -> Certificates<'_, C> {
         Certificates::new(self.client)
+    }
+
+    /// To call [Roles] group related APIs using this client.
+    pub fn roles(&self) -> Roles<'_, C> {
+        Roles::new(self.client)
+    }
+
+    /// To call [Groups] group related APIs using this client.
+    pub fn groups(&self) -> Groups<'_, C> {
+        Groups::new(self.client)
     }
 }
